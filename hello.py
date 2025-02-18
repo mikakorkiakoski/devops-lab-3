07a05e538b18357aa393fc2b49f220aed763a0cd
mikakorkiakoski <mika.korkiakoski@icloud.com>
2025-02-18 13:36:13 +0000

    Final modification to hello.py

--- hello.py
+++ hello.py
@@ -2,4 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return '<p>Hello, World!</p>'
+    return '<p>Hello, World! <a href="/about">Link to About</a></p>'
+
+@app.route('/about')
+def about():
+    return '<p>This is a Flask web app running in a Linux VM.</p>'
